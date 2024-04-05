ce46180cdab39c068991eff2382842faa0665e1b
Korobprog <korobprog@gmail.com>
2024-04-05 20:52:30 +1000

    services api

---
+++ src/app/services/postsApi.ts
@@ -0,0 +1,45 @@
+import type { Post } from "../types"
+import { api } from "./api"
+
+export const postApi = api.injectEndpoints({
+  endpoints: builder => ({
+    createPost: builder.mutation<Post, { content: string }>({
+      query: postData => ({
+        url: "/posts",
+        method: "POST",
+        body: postData,
+      }),
+    }),
+    getALLPosts: builder.query<Post[], void>({
+      query: () => ({
+        url: "/posts",
+        method: "GET",
+      }),
+    }),
+    getPostById: builder.query<Post, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "GET",
+      }),
+    }),
+    deletePosts: builder.mutation<void, string>({
+      query: id => ({
+        url: `/posts/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+})
+
+export const {
+  useCreatePostMutation,
+  useGetALLPostsQuery,
+  useGetPostByIdQuery,
+  useDeletePostsMutation,
+  useLazyGetALLPostsQuery,
+  useLazyGetPostByIdQuery,
+} = postApi
+
+export const {
+  endpoints: { createPost, getALLPosts, getPostById, deletePosts },
+} = postApi
---
+++ src/app/services/userApi.ts
@@ -0,0 +1,60 @@
+import type { User } from "../types"
+import { api } from "./api"
+
+export const userApi = api.injectEndpoints({
+  endpoints: builder => ({
+    login: builder.mutation<
+      { token: string },
+      { email: string; pasword: string }
+    >({
+      query: userData => ({
+        url: "/login",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    register: builder.mutation<
+      { email: string; password: string; name: string },
+      { email: string; password: string; name: string }
+    >({
+      query: userData => ({
+        url: "/register",
+        method: "POST",
+        body: userData,
+      }),
+    }),
+    current: builder.query<User, void>({
+      query: () => ({
+        url: `/current`,
+        method: "GET",
+      }),
+    }),
+    getuserById: builder.query<User, string>({
+      query: id => ({
+        url: `/users/${id}`,
+        method: "GET",
+      }),
+    }),
+    updateUser: builder.mutation<User, { userData: FormData; id: string }>({
+      query: ({ userData, id }) => ({
+        url: `/users/${id}`,
+        method: "PUT",
+        body: userData,
+      }),
+    }),
+  }),
+})
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useCurrentQuery,
+  useLazyCurrentQuery,
+  useLazyGetuserByIdQuery,
+  useGetuserByIdQuery,
+  useUpdateUserMutation,
+} = userApi
+
+export const {
+  endpoints: { login, register, current, getuserById, updateUser },
+} = userApi
