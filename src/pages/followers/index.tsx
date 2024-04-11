91cb9cddf71d0b8c6eb3b526794f71143edc221b
Korobprog <korobprog@gmail.com>
2024-04-11 21:48:11 +1000

    finish following followers pages

--- src/pages/followers/index.tsx
+++ src/pages/followers/index.tsx
@@ -1,7 +1,34 @@
 import React from "react"
+import { useSelector } from "react-redux"
+import { selectCurrent } from "../../features/user/userSlice"
+import { Link } from "react-router-dom"
+import { Card, CardBody } from "@nextui-org/react"
+import { User } from "../../components/user"
 
 const Followers = () => {
-  return <div> Followers</div>
+  const currentUser = useSelector(selectCurrent)
+  if (!currentUser) {
+    return null
+  }
+  return currentUser.followers.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.followers.map(user => (
+        <Link to={`/user/${user.follower.id}`} key={user.follower.id}>
+          <Card>
+            <CardBody className="block">
+              <User
+                name={user.follower.name ?? ""}
+                avatarUrl={user.follower.avatarUrl ?? ""}
+                description={user.follower.email ?? ""}
+              />
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h1>У Вас нет подписчиков</h1>
+  )
 }
 
 export default Followers
